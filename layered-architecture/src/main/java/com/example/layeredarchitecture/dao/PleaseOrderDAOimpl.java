dd06242c9f819b413f0c9467d5c53563eaf2f012
loshaniramsha <loshanikottagoda@gmail.com>
2023-12-18 23:26:23 +0530

    Add

--- layered-architecture/src/main/java/com/example/layeredarchitecture/dao/PleaseOrderDAOimpl.java
+++ layered-architecture/src/main/java/com/example/layeredarchitecture/dao/PleaseOrderDAOimpl.java
@@ -13,12 +13,14 @@ public class PleaseOrderDAOimpl {
         pstm.setString(1, code);
         return pstm.executeQuery().next();
     }
+
     public boolean existsCustomer(String id) throws SQLException, ClassNotFoundException {
         Connection connection = DBConnection.getDbConnection().getConnection();
         PreparedStatement pstm = connection.prepareStatement("SELECT id FROM Customer WHERE id=?");
         pstm.setString(1, id);
         return pstm.executeQuery().next();
     }
+
     public String newOrderId() throws SQLException, ClassNotFoundException {
         Connection connection = DBConnection.getDbConnection().getConnection();
         Statement stm = connection.createStatement();
@@ -26,6 +28,7 @@ public class PleaseOrderDAOimpl {
 
         return rst.next() ? String.format("OID-%03d", (Integer.parseInt(rst.getString("oid").replace("OID-", "")) + 1)) : "OID-001";
     }
+
     public void loadAllCustomer(JFXComboBox<String> cmbCustomerId) throws SQLException, ClassNotFoundException {
         Connection connection = DBConnection.getDbConnection().getConnection();
         Statement stm = connection.createStatement();
@@ -44,6 +47,7 @@ public class PleaseOrderDAOimpl {
             cmbItemCode.getItems().add(rst.getString("code"));
         }
     }
+
     public ItemDTO findItem(String code) throws SQLException, ClassNotFoundException {
         Connection connection = DBConnection.getDbConnection().getConnection();
         PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Item WHERE code=?");
@@ -52,4 +56,27 @@ public class PleaseOrderDAOimpl {
         rst.next();
         return new ItemDTO(code, rst.getString("description"), rst.getBigDecimal("unitPrice"), rst.getInt("qtyOnHand"));
     }
+
+//    public boolean saveOrder() throws SQLException, ClassNotFoundException {
+//        Connection connection = null;
+//
+//            connection = DBConnection.getDbConnection().getConnection();
+//            PreparedStatement stm = connection.prepareStatement("SELECT oid FROM `Orders` WHERE oid=?");
+//            stm.setString(1, orderId);
+//            /*if order id already exist*/
+//            if (stm.executeQuery().next()) {
+//
+//            }
+//        connection.setAutoCommit(false);
+//        stm = connection.prepareStatement("INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)");
+//        stm.setString(1, orderId);
+//        stm.setDate(2, Date.valueOf(orderDate));
+//        stm.setString(3, customerId);
+//
+//        if (stm.executeUpdate() != 1) {
+//            connection.rollback();
+//            connection.setAutoCommit(true);
+//            return false;
+//        }
+//    }
 }
